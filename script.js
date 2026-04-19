de388e49ac55926a0a1b58959d1263191fd6356d
imroniskandarov240@gmail.com <imroniskandarov240@gmail.com>
2026-04-19 17:16:23 +0500

    oclock

---
+++ script.js
@@ -0,0 +1,33 @@
+function klok(){
+const now = new Date();
+const hours = String(now.getHours()).padStart(2, "0");
+const minutes = String(now.getMinutes()).padStart(2, "0");
+const seconds = String(now.getSeconds()).padStart(2, "0");
+document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(klok, 1000);
+
+klok();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
